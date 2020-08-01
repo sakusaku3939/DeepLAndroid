d2aa3105271b8707c2d86c319b989f991c484958
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-02 03:41:52 +0900

    initial commit

---
+++ app/src/main/java/com/example/deeplviewer/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.deeplviewer
+
+import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setCookie("https://www.deepl.com/translator"
+                , "privacySettings=%7B%22v%22%3A%221%22%2C%22t%22%3A1596240000%2C%22consent%22%3A%5B%22NECESSARY%22%2C%22PERFORMANCE%22%2C%22COMFORT%22%5D%7D;" +
+                "domain=.deepl.com;" +
+                "path=/;" +
+                "max-age=31536000;")
+
+        val webView: WebView = findViewById(R.id.webview)
+        val webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:" +
+                        "\$('#dl_translator').siblings().hide();" +
+                        "\$('.dl_header_menu_v2__buttons__menu').hide();" +
+                        "\$('footer').hide();" +
+                        "\$('a').css('pointer-events','none');" +
+                        "")
+            }
+        }
+        webView.webViewClient = webViewClient
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.deepl.com/translator")
+    }
+}
