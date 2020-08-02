21b1d8cde3f608ac8b5a24a37f6c9fe033b6f357
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-02 16:23:30 +0900

    ボタンのタップハイライトを非表示

--- app/src/main/java/com/example/deeplviewer/MainActivity.kt
+++ app/src/main/java/com/example/deeplviewer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.deeplviewer
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -15,11 +16,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val cookieManager = CookieManager.getInstance()
-        cookieManager.setCookie("https://www.deepl.com/translator"
-                , "privacySettings=%7B%22v%22%3A%221%22%2C%22t%22%3A1596240000%2C%22consent%22%3A%5B%22NECESSARY%22%2C%22PERFORMANCE%22%2C%22COMFORT%22%5D%7D;" +
-                "domain=.deepl.com;" +
-                "path=/;" +
-                "max-age=31536000;")
+        cookieManager.setCookie(
+            "https://www.deepl.com/translator",
+            "privacySettings=%7B%22v%22%3A%221%22%2C%22t%22%3A1596240000%2C%22consent%22%3A%5B%22NECESSARY%22%2C%22PERFORMANCE%22%2C%22COMFORT%22%5D%7D;" +
+                    "domain=.deepl.com;" +
+                    "path=/;" +
+                    "max-age=31536000;"
+        )
 
         val webView: WebView = findViewById(R.id.webview)
         val webViewClient = object : WebViewClient() {
@@ -28,18 +31,22 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
                 return true
             }
+
             override fun onPageFinished(view: WebView, url: String) {
-                view.loadUrl("javascript:" +
-                        "\$('#dl_translator').siblings().hide();" +
-                        "\$('.dl_header_menu_v2__buttons__menu').hide();" +
-                        "\$('footer').hide();" +
-                        "\$('a').css('pointer-events','none');" +
-                        "")
+                view.loadUrl(
+                    "javascript:" +
+                            "\$('button').css('-webkit-tap-highlight-color','rgba(0, 0, 0, 0)');" +
+                            "\$('#dl_translator').siblings().hide();" +
+                            "\$('.dl_header_menu_v2__buttons__menu').hide();" +
+                            "\$('footer').hide();" +
+                            "\$('a').css('pointer-events','none');"
+                )
                 webView.alpha = 1.0F
             }
         }
-        webView.webViewClient = webViewClient
+        @SuppressLint("SetJavaScriptEnabled")
         webView.settings.javaScriptEnabled = true
+        webView.webViewClient = webViewClient
         webView.loadUrl("https://www.deepl.com/translator")
     }
 }
