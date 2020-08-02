a1f899200d93add0ccf0fc583ce3b229fd1794da
AokiYuki <yuukiyuuki223@gmail.com>
2020-08-02 19:15:07 +0900

    クリップボードにコピーする処理実装

--- app/src/main/java/com/example/deeplviewer/MainActivity.kt
+++ app/src/main/java/com/example/deeplviewer/MainActivity.kt
@@ -65,9 +65,11 @@ class MainActivity : AppCompatActivity() {
 class WebAppInterface(private val context: Context) {
     @JavascriptInterface
     fun copyClipboard(text: String) {
-        val clipboard: ClipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboard: ClipboardManager =
+            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip: ClipData = ClipData.newPlainText("translation_text", text)
         clipboard.setPrimaryClip(clip)
-        Toast.makeText(context, context.getString(R.string.copy_clipboard), Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, context.getString(R.string.copy_clipboard), Toast.LENGTH_SHORT)
+            .show()
     }
 }
