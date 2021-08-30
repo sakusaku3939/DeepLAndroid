6c1dca456bda70bbc45794c82023aa1b97d22425
S-H-Y-A <yamada0@hotmail.co.jp>
2021-08-30 13:56:14 +0900

    Add method to remove old App Shortcuts.

--- app/src/main/java/com/example/deeplviewer/App.kt
+++ app/src/main/java/com/example/deeplviewer/App.kt
@@ -2,7 +2,9 @@ package com.example.deeplviewer
 
 import android.app.Application
 import android.content.Context
+import android.content.pm.ShortcutManager
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDelegate
 
 class App : Application() {
@@ -10,6 +12,9 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         switchTheme()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            removeOldAppShortcut()
+        }
     }
 
     private fun switchTheme() {
@@ -24,5 +29,15 @@ class App : Application() {
             darkThemeMode = AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY.toString()
         }
         AppCompatDelegate.setDefaultNightMode(darkThemeMode.toInt())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N_MR1)
+    private fun removeOldAppShortcut() {
+        val shortcutManager = getSystemService(ShortcutManager::class.java)
+        shortcutManager.removeDynamicShortcuts(
+            listOf(
+                "theme"//version 5.0(17) Used to launch this app with darkTheme or lightTheme.
+            )
+        )
     }
 }
