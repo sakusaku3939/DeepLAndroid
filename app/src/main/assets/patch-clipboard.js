25a56aec41d06ba2eff1bae1ab5642198532a917
Aokiti <yuukiyuuki223@gmail.com>
2022-01-05 19:53:52 +0900

    Fixed a bug that prevented text copying from working. #23

--- app/src/main/assets/patch-clipboard.js
+++ app/src/main/assets/patch-clipboard.js
@@ -1,4 +1,8 @@
-$('.lmt__translations_as_text__copy_button, .lmt__target_toolbar__copy').on('click',function() {
-    const text = $('.lmt__translations_as_text__text_btn').eq(0).text();
-    Android.copyClipboard(text);
+$('.lmt__translations_as_text').on('DOMSubtreeModified propertychange', function () {
+    const copyButton = $('.lmt__translations_as_text__copy_button, .lmt__target_toolbar__copy');
+    copyButton.off('click');
+    copyButton.on('click', function () {
+        const text = $('.lmt__translations_as_text__text_btn').eq(0).text();
+        Android.copyClipboard(text);
+    });
 });
