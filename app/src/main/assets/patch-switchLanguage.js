ea14d8ff8b3d933715aaced8daed5c8b9b0c06ae
Aokiti <yuukiyuuki223@gmail.com>
2022-01-08 02:06:53 +0900

    Fixed the problem that strings after / are not translated when switching languages. #60

--- app/src/main/assets/patch-switchLanguage.js
+++ app/src/main/assets/patch-switchLanguage.js
@@ -1,10 +1,10 @@
 if (!$('#lang_switch').length) {
     $('html > head').append($('<style>#lang_switch.switched svg {transform:scaleY(-1)}</style>'));
-    $(Android.getAssetsText('switchLanguage.html')).click(function(){
+    $(Android.getAssetsText('switchLanguage.html')).click(function () {
         this.classList.contains("switched") ? this.classList.remove("switched") : this.classList.add("switched");
         window.location = window.location.href.split('#')[0] +
             '#' + document.getElementsByClassName('lmt__language_select lmt__language_select--target')[0].getAttribute('dl-selected-lang').split('-')[0] +
             '/' + document.getElementsByClassName('lmt__language_select lmt__language_select--source')[0].getAttribute('dl-selected-lang').split('-')[0] +
-            '/' + encodeURI(document.getElementsByClassName('lmt__textarea lmt__target_textarea lmt__textarea_base_style')[0].value);
+            '/' + encodeURI(document.getElementsByClassName('lmt__textarea lmt__target_textarea lmt__textarea_base_style')[0].value.replace("/", "\\/"));
     }).prependTo($('.lmt__language_container')[1]);
 }
