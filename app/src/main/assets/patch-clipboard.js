d6986c4a93fd0878579b0fe29e7442c3d4aca3fc
Aokiti <yuukiyuuki223@gmail.com>
2022-04-11 00:30:53 +0900

    Fixed the copy button in the new design to work.

--- app/src/main/assets/patch-clipboard.js
+++ app/src/main/assets/patch-clipboard.js
@@ -1,8 +1,19 @@
-$('.lmt__translations_as_text').on('DOMSubtreeModified propertychange', function () {
-    const copyButton = $('.lmt__translations_as_text__copy_button, .lmt__target_toolbar__copy');
+if ($('.lmt').length) {
+    /* Old design */
+    $('.lmt__translations_as_text').on('DOMSubtreeModified propertychange', function () {
+        const copyButton = $('.lmt__translations_as_text__copy_button, .lmt__target_toolbar__copy');
+        copyButton.off('click');
+        copyButton.on('click', function () {
+            const text = $('.lmt__translations_as_text__text_btn').eq(0).text();
+            Android.copyClipboard(text);
+        });
+    });
+} else {
+    /* New design */
+    var copyButton = $('.IconButton-module--button--1W1gd');
     copyButton.off('click');
     copyButton.on('click', function () {
-        const text = $('.lmt__translations_as_text__text_btn').eq(0).text();
+        const text = $(".TargetTextInput-module--textarea--1dPfl").val();
         Android.copyClipboard(text);
     });
-});
+}
