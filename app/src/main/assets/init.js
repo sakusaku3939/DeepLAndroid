ac86dc93c7089f0756a4a4ac32dd74fdff3a7f87
Aokiti <yuukiyuuki223@gmail.com>
2022-09-11 08:28:05 +0900

    Fixed a bug that the menu button does not hide

--- app/src/main/assets/init.js
+++ app/src/main/assets/init.js
@@ -1,9 +1,9 @@
-$('body').bind('DOMNodeInserted', () => {
+$('[class^="TranslatorPage-module--otherSections--"]').hide();
+$('a').css('pointer-events', 'none');
+$('*').css('-webkit-tap-highlight-color', 'rgba(0,0,0,.1)');
+$('footer').hide();
+
+$(function () {
     $('[class^="menuButton-module--menuButton--"]').hide();
     $('[class^="productNavigation-module--container--"]').children().not('[class^="productNavigation-module--translatorLink--"]').hide();
-    $('a').css('pointer-events', 'none');
-    $('*').css('-webkit-tap-highlight-color', 'rgba(0,0,0,.1)');
 });
-
-$('[class^="TranslatorPage-module--otherSections--"]').hide();
-$('footer').hide();
