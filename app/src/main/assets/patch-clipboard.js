ccfd4cbfa7c5c6b29b202e3942d30fa8bb222659
Aokiti <yuukiyuuki223@gmail.com>
2022-11-29 06:09:52 +0900

    Fixed a bug the copy button does not work

--- app/src/main/assets/patch-clipboard.js
+++ app/src/main/assets/patch-clipboard.js
@@ -1,6 +1,9 @@
-var copyButton = $('.Target--Dwohg').eq(2);
-copyButton.off('click');
-copyButton.on('click', function () {
-    const text = $('[class^="TextInput-module--textarea--"]').last().val();
-    Android.copyClipboard(text);
-});
+var copyButton = $('.Target--Dwohg');
+
+if (copyButton.length > 1) {
+    copyButton.last().off('click');
+    copyButton.last().on('click', function () {
+        const text = $('[class^="TextInput-module--textarea--"]').last().val();
+        Android.copyClipboard(text);
+    });
+}
