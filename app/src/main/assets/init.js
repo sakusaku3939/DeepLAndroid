76305e2d0e401f41805b2713dd0cfcf0b8687ff5
Aokiti <yuukiyuuki223@gmail.com>
2022-11-29 05:56:55 +0900

    Remove load listener

--- app/src/main/assets/init.js
+++ app/src/main/assets/init.js
@@ -3,7 +3,5 @@ $('a').css('pointer-events', 'none');
 $('*').css('-webkit-tap-highlight-color', 'rgba(0,0,0,.1)');
 $('footer').hide();
 
-$(function () {
-    $('[class^="menuButton-module--menuButton--"]').hide();
-    $('[class^="productNavigation-module--container--"]').children().not('[class^="productNavigation-module--translatorLink--"]').hide();
-});
+$('[class^="menuButton-module--menuButton--"]').hide();
+$('[class^="productNavigation-module--container--"]').children().not('[class^="productNavigation-module--translatorLink--"]').hide();
