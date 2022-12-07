8b3527b762fd9b69d456f01713718e0e7f60150c
crimera <stevendavemiranda2@gmail.com>
2022-12-07 19:23:50 +0800

    improved selecting of the nodes

--- app/src/main/assets/patch-ads.js
+++ app/src/main/assets/patch-ads.js
@@ -1,7 +1,2 @@
-// old method of removing the pro banner
-document.getElementsByClassName("TranslatorPage-module--otherSections--Ko+6x")[0].hidden = true;
-// new method of removing the pro banner
-document.body.childNodes[0].childNodes[0].childNodes[2].childNodes[2].remove();
-
-//remove the toolbar
-document.getElementsByClassName("pageHeader-module--container--DugaI")[0].hidden = true;
+document.body.childNodes[0].childNodes[0].childNodes[2].childNodes[2].hidden = true;
+document.body.childNodes[0].childNodes[0].childNodes[0].hidden = true;
