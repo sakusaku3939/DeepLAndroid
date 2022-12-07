eb40e2b1dcf812f9e661dd38c672e1010e0114c0
crimera <44558091+crimera@users.noreply.github.com>
2022-12-07 17:31:58 +0800

    Update patch-ads.js
    added a better method for removing the ad banner

--- app/src/main/assets/patch-ads.js
+++ app/src/main/assets/patch-ads.js
@@ -1,2 +1,7 @@
+// old method of removing the pro banner
 document.getElementsByClassName("TranslatorPage-module--otherSections--Ko+6x")[0].hidden = true;
+// new method of removing the pro banner
+document.body.childNodes[0].childNodes[0].childNodes[2].childNodes[2].remove();
+
+//remove the toolbar
 document.getElementsByClassName("pageHeader-module--container--DugaI")[0].hidden = true;
