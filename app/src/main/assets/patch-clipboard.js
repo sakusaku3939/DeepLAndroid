6d690a488fd7f4cc6a1f67c435dc13b2cf3afcb4
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-06-23 00:29:47 +0900

    Since the copy button now works on WebView, the original copy method has been discontinued

--- app/src/main/assets/patch-clipboard.js
+++ app/src/main/assets/patch-clipboard.js
@@ -4,6 +4,8 @@ if (copyButton.length > 1) {
     copyButton.last().off('click');
     copyButton.last().on('click', function () {
         const text = $('[class^="TextInput-module--textarea--"]').last().val();
+        /* Discontinued because the copy button now works on WebView as well
         Android.copyClipboard(text);
+        */
     });
 }
