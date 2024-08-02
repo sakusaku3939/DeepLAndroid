6cb2f0705fd499e5003e5c7940a46399ab4b139a
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-08-03 01:54:34 +0900

    Fixed a bug that elements disappear when tablet

--- app/src/main/assets/init.js
+++ app/src/main/assets/init.js
@@ -5,7 +5,6 @@ $('head').append(`<style>
     [class*="LogoLink-module--translatorLink--"] ~ div,
     [class^="ProductNavigation-module--container--"] + div,
     [class^="SideMenuButton-module--menuButton--"],
-    [class^="BasePageHeader-module--exContainer--"] + div > div:first-child,
     h2#translation-modes-heading + div > div:first-child,
     div[data-testid="translator"] span:nth-child(3),
     footer,
