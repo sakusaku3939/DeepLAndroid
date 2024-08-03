7101c5dd41e43f839d9b8bb12909e3ec8e4c507d
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-08-03 16:16:28 +0900

    Added a toolbar

--- app/src/main/assets/init.js
+++ app/src/main/assets/init.js
@@ -2,15 +2,14 @@ $('a').css('pointer-events', 'none');
 $('*').css('-webkit-tap-highlight-color', 'rgba(0,0,0,.1)');
 
 $('head').append(`<style>
-    [class*="LogoLink-module--translatorLink--"] ~ div,
-    [class^="ProductNavigation-module--container--"] + div,
-    [class^="SideMenuButton-module--menuButton--"],
+    header,
+    nav[class^="md"],
     h2#translation-modes-heading + div > div:first-child,
     div[data-testid="translator"] span:nth-child(3),
     footer,
     div[data-testid="chrome-extension-toast"],
     div[data-testid="firefox-extension-toast"],
-    div[id^="cookieBanner"],
+    div[id="cookieBanner"],
     aside {
         display: none !important;
         visibility: hidden;
