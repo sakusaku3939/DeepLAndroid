3433d3155cdb161e4c4c92332ce73669fcf5a2bd
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-11-09 01:27:40 +0900

    Update annotations to match SDK upgrades

--- app/src/main/java/com/example/deeplviewer/service/QSTileService.kt
+++ app/src/main/java/com/example/deeplviewer/service/QSTileService.kt
@@ -1,5 +1,7 @@
 package com.example.deeplviewer.service
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
 import android.content.Intent
 import android.os.Build
 import android.service.quicksettings.TileService
@@ -7,11 +9,18 @@ import androidx.annotation.RequiresApi
 import com.example.deeplviewer.activity.FloatingTextSelection
 
 @RequiresApi(Build.VERSION_CODES.N)
+@TargetApi(Build.VERSION_CODES.N)
 class QSTileService : TileService() {
     override fun onClick() {
         val intent = Intent(applicationContext, FloatingTextSelection::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.putExtra(Intent.EXTRA_TEXT, "")
-        startActivityAndCollapse(intent)
+
+        @SuppressLint("StartActivityAndCollapseDeprecated")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startActivity(intent)
+        } else {
+            startActivityAndCollapse(intent)
+        }
     }
 }
